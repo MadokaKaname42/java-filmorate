372cb588d0394d1a759b78a16d67980ddc466a1c
madoka <obivanyakenobi@gmail.com>
2025-09-09 21:30:17 +0300

    Repositories

---
+++ src/main/java/ru/yandex/practicum/filmorate/storage/DbRepository/UserDbRepository.java
@@ -0,0 +1,73 @@
+package ru.yandex.practicum.filmorate.storage.DbRepository;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.context.annotation.Primary;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.RowMapper;
+import org.springframework.stereotype.Repository;
+import ru.yandex.practicum.filmorate.model.User;
+import ru.yandex.practicum.filmorate.storage.UserStorage;
+
+import java.util.Collection;
+import java.util.List;
+import java.util.Optional;
+
+@Slf4j
+@Repository
+@Primary
+public class UserDbRepository extends BaseRepository<User> implements UserStorage {
+    private static final String FIND_ALL_QUERY = "SELECT * FROM users";
+    private static final String FIND_BY_ID_QUERY = "SELECT * FROM users WHERE user_id = ?";
+    private static final String INSERT_QUERY = "INSERT INTO users (email, login, name, birthday)" +
+            "VALUES (?, ?, ?, ?)";
+    private static final String UPDATE_QUERY = "UPDATE users SET email = ?, login = ?, name = ?, birthday = ? WHERE user_id = ?";
+    private static final String DELETE_QUERY = "DELETE FROM users WHERE user_id = ?";
+
+
+    public UserDbRepository(JdbcTemplate jdbc, RowMapper<User> mapper) {
+        super(jdbc, mapper);
+    }
+
+    @Override
+    public Collection<User> findAll() {
+        return findMany(FIND_ALL_QUERY);
+    }
+
+    @Override
+    public Optional<User> findById(long userId) {
+        return findOne(FIND_BY_ID_QUERY, userId);
+    }
+
+    @Override
+    public User save(User user) {
+        log.info("");
+        long id = insert(
+                INSERT_QUERY,
+                user.getEmail(),
+                user.getLogin(),
+                user.getName(),
+                user.getBirthday());
+        user.setId(id);
+        log.info("");
+        return user;
+    }
+
+
+    @Override
+    public void deleteById(long userId) {
+        delete(DELETE_QUERY, userId);
+    }
+
+    @Override
+    public User update(User user) {
+        update(
+                UPDATE_QUERY,
+                user.getEmail(),
+                user.getLogin(),
+                user.getName(),
+                user.getBirthday(),
+                user.getId()
+        );
+        return user;
+    }
+}
